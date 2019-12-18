8f9419fc25b37414ccd39ff46960cb46f54f4a4b
Uriel Ricardo <winchesteruriel@gmail.com>
2019-12-18 16:12:16 -0300

    Create portscan.sh

---
+++ portscan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+hosts=(
+"google.com"
+"xvideos.com"
+)
+for host in "${hosts[@]}"
+do
+   for port in {21,22,80,443}
+   do
+      echo "(echo > /dev/tcp/$host/$port) 2>/dev/null && echo Host:$host Port:$port is open"
+   done
+done | parallel -k -j 32
